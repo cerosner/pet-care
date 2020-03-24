7dec8fc817287de67d905fbaf22182d11e45e905
Cassie Rosner <cerosner@gmail.com>
2020-03-24 00:20:58 -0400

    :pencil: Comment edits
    Refactors codebase to better self-document.

--- db/test/connect.js
+++ db/test/connect.js
@@ -1,25 +1,28 @@
+/*
+  Change into the `db` directory before
+  executing `npm run test` in the terminal.
+*/
+
 const mongoose = require('mongoose')
 const DB = require('../../package.json').name
 
-// Use ES6 Promise Library
+// Uses ES6 Promise Library
 mongoose.Promise = global.Promise
 
-// Connect to database before running tests
+// Connects to database
 before(function (done) {
-  // Connecting to MongoDb
   mongoose.connect(`mongodb://localhost/${DB}`, {
     useNewUrlParser: true, useUnifiedTopology: true
   })
 
   mongoose.connection.once('open', function () {
     console.log(`>> Connected to ${DB} database! (:`)
-
     done()}).on('error', function () {
     console.log(`! ERROR: db ${DB} connection failed >>`, error)
   })
 })
 
-// Delete dummy data before each test
+// Deletes dummy data
 beforeEach(function (done) {
   mongoose.connection.collections.owners.drop(function () {
     done()
