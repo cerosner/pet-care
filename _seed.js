98ad61ef2156094c057c123fcb503e95f7c5f991
Cassie Rosner <cerosner@gmail.com>
2020-04-17 21:05:01 -0400

    :seedling: Basic Seed
    Basic seed file for tests.

---
+++ _seed.js
@@ -0,0 +1,25 @@
+const SERVICE_FEE = {
+  walk: 20,
+  visit: 15,
+  overnight: 45
+}
+
+const CLIENT = {
+  name: 'Charlotte Azaceta'
+}
+
+const PET = {
+  name: 'Brody',
+  category: 'dog'
+}
+
+const SERVICE = {
+  category: 'walk',
+  fee: SERVICE_FEE['category']
+}
+
+module.exports = {
+  CLIENT,
+  PET,
+  SERVICE
+}
